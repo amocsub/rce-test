36894db57af4df7e01c51481a5ed79744a79ee03
Davinci Lisapaly <davincilisapaly@gmail.com>
2024-05-08 11:46:20 +0200

    Update program.cs

--- program.cs
+++ program.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program
 {
+
+    private static string WindowsExecutableFileName => "wkhtmltopdf-0.12.6.0.exe";
+
     static void Main(string[] args)
     {
+        Cli.Wrap("wkhtmltopdf-0.12.6.0.exe");
+        var executable = "wkhtmltopdf-0.12.6.0.exe"; 
         Console.WriteLine("Hello, World!");
     }
 }
